50132a2d683904f0c527d5b2802e23611278e7e6
dnjawm19 <dnjawm19@naver.com>
2024-07-30 17:51:29 +0900

    feat: 폴더별 관심 상품 조회 구현

--- src/main/java/com/sparta/myselectshop/controller/ProductController.java
+++ src/main/java/com/sparta/myselectshop/controller/ProductController.java
@@ -38,7 +38,21 @@ public class ProductController {
     }
 
     @PostMapping("/products/{productId}/folder")
-    public void addFolder(@PathVariable Long productId, @RequestParam long folderId, @AuthenticationPrincipal UserDetailsImpl userDetails){
-        productService.addFolder(productId,folderId,userDetails.getUser());
+    public void addFolder(@PathVariable Long productId, @RequestParam long folderId, @AuthenticationPrincipal UserDetailsImpl userDetails) {
+        productService.addFolder(productId, folderId, userDetails.getUser());
+    }
+
+    @GetMapping("/folders/{folderId}/products")
+    public Page<ProductResponseDto> getProductsInFolder(
+        @PathVariable Long folderId,
+        @AuthenticationPrincipal UserDetailsImpl userDetails,
+        @RequestParam("page") int page,
+        @RequestParam("size") int size,
+        @RequestParam("sortBy") String sortBy,
+        @RequestParam("isAsc") boolean isAsc
+    ) {
+        return productService.getProductsInFolder(
+            folderId, page - 1, size, sortBy, isAsc, userDetails.getUser()
+        );
     }
 }
